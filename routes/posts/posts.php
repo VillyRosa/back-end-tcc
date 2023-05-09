797f0c81796ef0030affaed8f07cb0237138bda6
Villy Rosa <82731258+VillyRosa@users.noreply.github.com>
2023-05-09 12:20:19 -0300

    Add files via upload

--- routes/posts/posts.php
+++ routes/posts/posts.php
@@ -12,7 +12,22 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $sql = $pdo->prepare('SELECT * FROM posts');
             $sql->execute();
 
-            return returnObject(['error' => false, 'qtd' => $sql->rowCount(), 'data' => $sql->fetchAll(PDO::FETCH_ASSOC)]);
+            $data = [];
+            
+            foreach ($sql->fetchAll(PDO::FETCH_ASSOC) as $line) {
+                $userid = $line['id_usuario'];
+                $json = file_get_contents("http://localhost/tcc/back-end/routes/user/user.php?userid=$userid");
+                $convert = json_decode($json, true);
+                array_push($data, [
+                    'id_post' => $line['id_post'],
+                    'conteudo_post' => $line['conteudo_post'],
+                    'datahora_post' => $line['datahora_post'],
+                    'curtidas_post' => $line['curtidas_post'],
+                    'user_post' => $convert['data'][0]
+                ]);
+            }
+
+            return returnObject(['error' => false, 'qtd' => $sql->rowCount(), 'data' => $data]);
 
         } catch (PDOException $e) {
             return returnObject(['error' => true, 'msgErro' => 'Erro ao se comunicar com o banco!']);
